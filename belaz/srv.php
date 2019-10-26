0fdefc325f0abeab5f0666263617df56186a1343
Rizvan Nazriev <RizvanNazri@users.noreply.github.com>
2019-10-26 19:13:02 +0300

    belaz first commit

---
+++ belaz/srv.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 
+    $tel = strip_tags($_POST['tel']);
+ 
+    $res = array();
+ 
+    if(empty($tel)) {
+        $res['error'] = "Нужно добавить номер телефона!";
+        echo json_encode($res);
+ 
+        exit();
+    }
+}
